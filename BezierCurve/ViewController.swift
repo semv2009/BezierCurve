6ebaba015df7680815e1acd73990a4a624a6ae4d
Семен Никулин <snikulin@spaceotechnologies.ru>
2016-10-20 16:40:45 +0700

    Initial Commit

---
+++ BezierCurve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BezierCurve
+//
+//  Created by Семен Никулин on 20.10.16.
+//  Copyright © 2016 niksemv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
